b01ec9895b8addb23f77f66e63ddf1405814f128
Bharath <bharathnbs01@gmail.com>
2022-09-02 11:41:48 +0530

    admin - movie crud change to livewire

--- resources/views/livewire/admin/movies/movie-update.blade.php
+++ resources/views/livewire/admin/movies/movie-update.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="card" style="width:30%; margin-left:500px; margin-top:20px; padding-bottom:20px;">
-        <div class="card-header" style="background-color:red"><h2 style="color:white; margin-left:100px;">Movie Update</h2></div>
+        <div class="card-header" style="background-color:#A66CFF"><h2 style="color:white; margin-left:100px;">Movie Update</h2></div>
         <div class="card-body">
             @if ($errors->any())
                 <div>
@@ -11,28 +11,26 @@
                         </ul>
                 </div>
             @endif
-            <form action="{{route('admin.movies.update', $movie->id)}}" method="post" enctype="multipart/form-data" class="form">
-                @csrf
-                <label >City Name</label><br>
-                <select name="theatre_id" class="form-control">
+                            <label >City Name</label><br>
+                <select wire:model="theatre_id" class="form-control">
                     @foreach($theatres as $theatre)
                     <option value="{{$theatre->id}}">{{$theatre->name}}</option>
                     @endforeach
                 </select><br>
                 <label >Movie Name</label><br>
-                <input type="text" name="name" value="{{old('name', $movie->name)}}" class="form-control"><br>   
+                <input type="text" wire:model="name" class="form-control"><br>   
                 <label >genres</label><br>
-                <input type="text" name="genres" value="{{old('name', $movie->genres)}}"  class="form-control"><br>
+                <input type="text" wire:model="genres" class="form-control"><br>
                 <label >time</label><br>
-                <input type="time" name="time" value="{{old('name', $movie->time)}}"  class="form-control"><br>
+                <input type="time" wire:model="time" class="form-control"><br>
                 <label >date</label><br>
-                <input type="date" name="date" value="{{old('name', $movie->date)}}"  class="form-control"><br>
+                <input type="date" wire:model="date" class="form-control"><br>
                 <label >price</label><br>
-                <input type="text" name="price" value="{{old('name', $movie->price)}}"  class="form-control"><br>
-                <input type="file" name="images" class="form-control"><br>
+                <input type="text" wire:model="price" class="form-control"><br>
+                <input type="file" wire:model="images" class="form-control"><br>
 
-                <input type="submit" value="submit" class="btn btn-primary" style="margin-left:170px;">
-            </form>
+                <input type="submit" wire:click="update" class="btn btn-primary" style="margin-left:170px;">
+    
         </div>
     </div>
 </div>
